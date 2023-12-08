c05a067fc7d5c9a693e68b3e9c136c21df44cb5e
johnsiak <149271195+johnsiak@users.noreply.github.com>
2023-12-08 12:27:58 +0200

    Update FeedbackService.js
    Delete duplicate code

--- service/FeedbackService.js
+++ service/FeedbackService.js
@@ -13,18 +13,3 @@ exports.feedbackPOST = function(body) {
     resolve();
   });
 }
-
-
-/**
- * Send feedback to developers through server
- * FR10 - The logged-in user must be able to contact the system's designers.  <br>
- *
- * body Feedback Feedback information
- * no response value expected for this operation
- **/
-exports.feedbackPOST = function(body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
-
